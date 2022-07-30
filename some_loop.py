a5c2cfcc5602a28ffe48ad0cf0cbb4598a7796d5
Tony Tan <tyx1999120755@gmail.com>
2022-07-30 18:50:16 -0400

    abc

---
+++ some_loop.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+for i in range(0,5):
+  print("abc")
+  print("abc")
+  print("abc")
+  print("")
+  print("")
+  print("abc")
+  print("abc")
+  print("")
+  print("")
+  print("abc")
+  print("abc")
+  print("")
+  print("")
+  
